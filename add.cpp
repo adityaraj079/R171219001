a77a2769c22a643985f211447ae09b54dfc7b5e2
adityaraj079 <55791929+adityaraj079@users.noreply.github.com>
2020-03-26 22:58:10 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -8,7 +8,13 @@ int main()
     cout << "Enter two integers: ";
     cin >> firstNumber >> secondNumber;
     sumOfTwoNumbers = firstNumber + secondNumber;
-    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;   
+    
+    //FROM HERE ITS FRM ADITYA RAJ
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+    diffOfTwoNumbers = firstNumber - secondNumber;
+    cout << firstNumber << " + " <<  secondNumber << " = " << diffOfTwoNumbers;   
 
     return 0;
 }
