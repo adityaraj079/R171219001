4d4c22b87c4663df6390b51ceff2440fd701cb91
Akshat <akshatgoyal1018@gmail.com>
2020-03-26 23:41:42 +0530

    Added Multiplication Function in add.cpp

--- add.cpp
+++ add.cpp
@@ -16,5 +16,10 @@ int main()
     diffOfTwoNumbers = firstNumber - secondNumber;
     cout << firstNumber << " + " <<  secondNumber << " = " << diffOfTwoNumbers;   
     //ADITYA RAJ'S edit is over till here only
+    //Akshat's edit start
+    cout<<"Enter Two integers for multiplication:";
+    cin >>firstNumber>>secondNumber;
+    cout<<"/n Product="<<firstNumber*secondNumber;
+    //Akshat's edit ends here
     return 0;
 }
