9936e14703bdef320dfb5ca601b4fc293d4c27b0
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-02 23:39:14 +0530

    Update cal.cpp

--- cal.cpp
+++ cal.cpp
@@ -18,22 +18,6 @@ int main()
             cout << num1+num2;
             break;
 
-        case '-':
-            cout << num1-num2;
-            break;
-
-        case '*':
-            cout << num1*num2;
-            break;
-
-        case '/':
-            cout << num1/num2;
-            break;
-
-        default:
-            // If the operator is other than +, -, * or /, error message is shown
-            cout << "Error! operator is not correct";
-            break;
     }
 
     return 0;
