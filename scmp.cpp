4293f5c13b02b66451688650255070bfedd7b6da
LUCIFER <57518019+AAKASH707@users.noreply.github.com>
2020-04-05 19:35:07 +0530

    Update scmp.cpp

--- scmp.cpp
+++ scmp.cpp
@@ -6,3 +6,16 @@ int data;
 Node *link; 
 }; 
 Node *top = Null; 
+                // from R171219001
+void pop(int *curr) 
+{ 
+if(*curr == 0) 
+cout<<“\nStack is EMPTY\n ”; 
+else 
+{ 
+Node *ptr = top; 
+top = top -> link; 
+delet(ptr); 
+*curr--; 
+} 
+}
